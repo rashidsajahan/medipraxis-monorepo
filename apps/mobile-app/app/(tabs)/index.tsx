ab793d1bbf49ecc168f5da437488221821aa36f3
Speedy <bimalshahasanthi@gmail.com>
2025-12-19 02:04:11 +0530

    feat: clone project and add config structure

--- apps/mobile-app/app/(tabs)/index.tsx
+++ apps/mobile-app/app/(tabs)/index.tsx
@@ -2,11 +2,12 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { text } from '@repo/config';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>{text}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
