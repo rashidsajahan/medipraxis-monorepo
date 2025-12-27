77df1cfe89bcf954bd13ca0ea65e93b313a6aed0
supunTE <supunedirisuriya2002@gmail.com>
2025-12-27 18:26:29 +0530

    refactor: remove unused Env type from routes and reorder task route methods

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
-import type { Env } from "../types";
 import tasks from "./tasks";
 
-const routes = new Hono<{ Bindings: Env }>().route("/tasks", tasks);
+const routes = new Hono().route("/tasks", tasks);
 
 export default routes;
--- apps/api/src/routes/tasks.ts
+++ apps/api/src/routes/tasks.ts
@@ -8,13 +8,13 @@ import { Hono } from "hono";
 import { TaskController } from "../controllers";
 
 const tasks = new Hono()
-  .post("/", zValidator("json", createTaskSchema), TaskController.createTask)
   .get("/", TaskController.getAllTasks)
   .get(
     "/:id",
     zValidator("json", taskDetailsSchema),
     TaskController.getTaskById
   )
+  .post("/", zValidator("json", createTaskSchema), TaskController.createTask)
   .put("/:id", zValidator("json", updateTaskSchema), TaskController.updateTask);
 
 export default tasks;
