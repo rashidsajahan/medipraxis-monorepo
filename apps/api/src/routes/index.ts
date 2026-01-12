93b0b16cf7a0d4e5ae56649ff757de0966eb114d
AmandhaPanagoda <amandhaisiri@gmail.com>
2026-01-12 12:46:10 +0530

    feat: routers for upload reports

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,15 +1,17 @@
 import { Hono } from "hono";
 import ai from "./ai";
+import clientReports from "./client_reports";
+import clients from "./clients";
 import slotWindows from "./slot_windows";
 import tasks from "./tasks";
-import clients from "./clients";
 
 const routes = new Hono()
-  
+
   .route("/tasks", tasks)
-  
+
   .route("/ai", ai)
   .route("/slot-windows", slotWindows)
-  .route("/clients", clients);
+  .route("/clients", clients)
+  .route("/client-reports", clientReports);
 
 export default routes;
