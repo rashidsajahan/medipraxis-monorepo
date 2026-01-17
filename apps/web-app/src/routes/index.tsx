b335b90dfc28fd5005fdaf7991fa33b38fbe8b69
lasanya-premarathne <kldpremarathne@gmail.com>
2026-01-18 00:01:47 +0530

    feat: test

--- apps/web-app/src/routes/index.tsx
+++ apps/web-app/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { WhatsAppClient, WhatsAppService } from "@repo/whatsapp";
 import { createFileRoute } from "@tanstack/react-router";
 import { FormDemo } from "../pages/FormDemo";
 
@@ -6,11 +7,76 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
+  const sendWhatsApp = async () => {
+    // const url = `https://graph.facebook.com/v20.0/997165740137495/messages`; // busy
+    const url = `https://graph.facebook.com/v20.0/977178835473013/messages`; // bot-test
+
+    const payload = {
+      messaging_product: "whatsapp",
+      to: 94763203204,
+      type: "template",
+      template: {
+        name: "hello_world",
+        // name: "appointment_confirmation",
+        language: { code: "en_US" },
+        // components: [
+        //   {
+        //     type: "body",
+        //     parameters: [
+        //       { type: "text", text: "John Doe" }, //
+        //       { type: "text", text: "Smith Well" },
+        //       { type: "text", text: "15th January" },
+        //       { type: "text", text: "4.00pm" },
+        //       { type: "text", text: "6" },
+        //       {
+        //         type: "text",
+        //         text: "https://www.medipraxis.com.lk/view-appointment/6",
+        //       },
+        //     ],
+        //   },
+        // ],
+      },
+    };
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer EAAJ51YouF1IBQWs7EWfsRf8hJozG5js519UXObGBcl5hWYW9Q3oyCr8GaQVkZBExjQkvxUCb5OfnZATLKrxk8CURg9KJYyOiziJKtuPLGaCW18fye1YjQ4kNSZAOR4WkHsB8EZALFZBB7D10wyjHekZBDsxOosefZCwoVB5uuYZAAITmAzZCUzBV4TWx4Tfd1ZBwZDZD`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.error("WhatsApp API error:", data);
+      throw new Error("Failed to send WhatsApp message");
+    }
+
+    return data;
+  };
   console.log("Index component rendering");
+
+  const waService = new WhatsAppService(new WhatsAppClient());
+
+  const sendWhatsAppService = async () => {
+    debugger;
+    await waService.sendAppointmentConfirmation("94770690506", {
+      clientName: "John",
+      practitionerName: "Silva",
+      date: "12 Feb 2026",
+      time: "10:30 AM",
+      apptNumber: "42",
+      apptLink: "https://www.medipraxis.com.lk/view-appointment/42",
+    });
+  };
+
   return (
     <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
       <h1>Form Page</h1>
       <FormDemo />
+      <button onClick={sendWhatsAppService}>Send WhatsApp Message</button>
     </div>
   );
 }
