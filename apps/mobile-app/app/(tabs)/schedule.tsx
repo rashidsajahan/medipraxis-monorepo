9a3f939ffc7ab95ed046d2e3ee9bcbbb8e8562bc
lasanya-premarathne <kldpremarathne@gmail.com>
2026-02-24 11:16:13 +0530

    fix: comment - folder structure adjustment

--- apps/mobile-app/app/(tabs)/schedule.tsx
+++ apps/mobile-app/app/(tabs)/schedule.tsx
@@ -5,8 +5,7 @@ import {
   AgendaSelectionType,
   CalendarComponent,
 } from "@/components/advanced";
-import { ViewAppointmentModal } from "@/components/advanced/shedule/ViewAppointmentModal";
-import { ViewReminderModal } from "@/components/advanced/shedule/ViewReminderModal";
+import { ViewAppointmentModal , ViewReminderModal} from "@/components/advanced/schedule";
 import { useGetTaskById } from "@/services/tasks/useGetTaskById";
 import { Color } from "@repo/config";
 import { useEffect, useState } from "react";
