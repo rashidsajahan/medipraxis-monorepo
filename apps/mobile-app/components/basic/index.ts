fc7eeac476e021ff0946ad9691e4581fa34eb4de
AmandhaPanagoda <amandhaisiri@gmail.com>
2026-03-15 14:12:38 +0530

    feat: add InlineDropdown component for client selection in request report

--- apps/mobile-app/components/basic/index.ts
+++ apps/mobile-app/components/basic/index.ts
@@ -1,5 +1,6 @@
 export * from "./Button.component";
 export * from "./Dropdown.component";
+export * from "./InlineDropdown.component";
 export * from "./Text.component";
 export * from "./TextInput.component";
 export * from "./ToggleInput.component";
